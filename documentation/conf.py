b072c00f9efc2fae82e6ed26302eaef0d561bbf2
Edin Arslanagic <edoarsla@gmail.com>
2016-12-04 20:41:49 -0600

    testing with structure

--- documentation/conf.py
+++ documentation/conf.py
@@ -18,30 +18,6 @@
 #
 #import os
 #import sys
-#sys.path.insert(0, os.path.abspath('.'))
-
-#import sphinx_rtd_theme
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-#if not on_rtd:  # only import and set the theme if we're building docs locally
-#    import sphinx_rtd_theme
-#    html_theme = 'sphinx_rtd_theme'
-#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-
-#html_theme_options = {
-#    'collapse_navigation': False,
-#    'display_version': False,
-#    'navigation_depth': 3,
-#}
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
